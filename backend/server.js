340b21a7c511e3cbb51114234dfac1d147480f27
Bradford Mashlum <bmashlum@gmail.com>
2018-11-11 19:39:28 -0500

    Added MLab support

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1/school');
+mongoose.connect('mongodb://bmashlum:2019Kuronos@ds121163.mlab.com:21163/paprogram');
 
 const connection = mongoose.connection;
 
